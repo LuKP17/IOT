a6bdb0978e8d479160e484f3973677193ea68dfb
m2gi-pottielu <lucas.pottier@etu.univ-grenoble-alpes.fr>
2025-04-08 15:37:03 +0200

    Init step3

---
+++ step3/uart-mmio.h
@@ -0,0 +1,51 @@
+/*
+ * Copyright: Olivier Gruber (olivier dot gruber at acm dot org)
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms
+ * of the GNU General Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program.
+ * If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef UART_MMIO_H_
+#define UART_MMIO_H_
+
+/**
+ * To fill this header file,
+ * look at the document describing the Versatile Application Board:
+ *
+ *    Versatile Application Baseboard for ARM926EJ-S (DUI0225)
+ */
+
+/*
+ * We need the base address for the different serial lines.
+ */
+
+#define UART0_BASE_ADDRESS ((void*)0x101F1000)
+#define UART1_BASE_ADDRESS ((void*)0x101F2000)
+#define UART2_BASE_ADDRESS ((void*)0x101F3000)
+
+/*
+ * Is the UART chipset a PL011?
+ * If so, we need the details for registers.
+ */
+#define UART_DR     0x000 // data register
+
+#define UART_FR     0x018 // flag register
+#define UART_FR_RXFE (1 << 4)   // receive FIFO is empty
+#define UART_FR_TXFF (1 << 5)   // transmit FIFO is empty
+
+#define UART_IMSC   0x038 // interrupt mask set/clear, to enable/diasble interrupts
+#define UART_IMSC_RXIM (1 << 4) // receive interrupt mask
+
+#define UART_RIS    0x03C // raw interrupt status register, what the device wants to do
+#define UART_MIS    0x040 // masked interrupt status register, UART_IMSC & UART_RIS
+
+#define UART_ICR    0x044 // interrupt clear register, clear UART_RIS bits 
+
+#endif /* UART_MMIO_H_ */
